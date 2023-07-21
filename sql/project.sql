e00fd8a71d5c0beb86f51fb0e5abca307368142b
Alan Miller <183872+alantmiller@users.noreply.github.com>
2023-07-21 15:49:42 -0700

    Update project.sql

--- sql/project.sql
+++ sql/project.sql
@@ -1,1 +1,26 @@
+-- Raw SQL
+CREATE TABLE writings (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  source_id INT,
+  title VARCHAR(255), 
+  body TEXT,
+  page_num INT NULL, 
+  created_date DATE NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
 
+-- Create table
+CREATE TABLE sources (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  source VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Populate data
+INSERT INTO sources (id, source)
+VALUES
+  (1, "Idle Thoughts"),
+  (2, "Memoirs"), 
+  (4, "My Dedication");
