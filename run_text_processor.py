d7f47d7155be16b4690e1574c3a7768aa68aa574
Alan Miller <183872+alantmiller@users.noreply.github.com>
2023-07-21 18:54:51 -0700

    Update run_text_processor.py

--- run_text_processor.py
+++ run_text_processor.py
@@ -1,1 +1,23 @@
+# Import the necessary classes and modules
+from src.text_file_processor import TextFileProcessor
+import json
 
+def main():
+    # Load configuration file
+    with open('config/config_development.json') as f:
+        config = json.load(f)
+    
+    # Extract configuration variables
+    source_folder = config['folders']['source'] 
+    dest_folder = config['folders']['destination']
+    max_files = config['max_files']
+
+    # Create an instance of TextFileProcessor
+    processor = TextFileProcessor(source_folder, dest_folder, max_files)
+
+    # Process files
+    processor.process_files()
+
+# If this script is run directly (not imported), call the main function
+if __name__ == "__main__":
+    main()
